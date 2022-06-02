814f935ae3f58cbe0593ff0e6d55ccd886da37f8
thestraightfrenchguy <edouard.finet@hotmail.co.uk>
2022-06-02 16:59:20 +0100

    Add a Header Navbar for home page

---
+++ components/HeaderBig.tsx
@@ -0,0 +1,29 @@
+import { WalletConnectButton } from "@solana/wallet-adapter-react-ui";
+import Image from "next/image";
+import Link from "next/link";
+
+const HeaderBig = () => {
+  return (
+    <header>
+      <Image src={"/abslogo.jpg"} alt="Abstratica logo" width={112} height={112}/>
+      <div>
+        <h1>Abstratica</h1>
+        <nav className="main-nav">
+          <ul>
+            <li><Link href={"/"}>Home</Link></li>
+            <li><Link href={"/collection/full-collection"}>Collection</Link></li>
+            <li><Link href={"/roadmap"}>Roadmap</Link></li>
+            <li><Link href={"/staking"}>Staking</Link></li>
+            <li><Link href={"/art-zone/raffles"}>1:1 Art Zone</Link></li>
+          </ul>
+        </nav>
+      </div>
+      
+      <div>
+        <WalletConnectButton />
+      </div>
+    </header>
+  );
+}
+
+export default HeaderBig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Footer from '../components/Footer'
+import HeaderBig from '../components/HeaderBig'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <HeaderBig />
       <Component {...pageProps} /> 
       <Footer />
     </>
