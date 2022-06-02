cacbaa52a3c6f7eb78179cefda17d205b9e87910
thestraightfrenchguy <edouard.finet@hotmail.co.uk>
2022-06-02 14:57:37 +0100

    add a footer

---
+++ components/Footer.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <p>Powered by</p>
+        <Image src="/footer/tensorflow.svg" alt="tensorflow" height={40} width={40} />
+        <Image src="/footer/solana.svg" alt="solana logo" height={40} width={40} />
+      </div>
+      <div>
+        the OG art project<br/>Since 14.8.2021
+      </div>
+      <div className="clickable">
+        <Link href={"https://dsc.gg/abstratica"}  rel="noopener noreferrer">
+          <a target={"_blank"}>
+            <Image src="/footer/discord.svg" alt="discord" height={40} width={40} />
+          </a>
+        </Link>
+        <Link href={"https://magiceden.io/marketplace/abstratica"}  rel="noopener noreferrer">
+          <a target={"_blank"}>
+            <Image src="/footer/magiceden.svg" alt="magiceden" height={40} width={160} />
+          </a>          
+        </Link>
+        <Link href={"https://twitter.com/AbstraticaNFT"}  rel="noopener noreferrer"> 
+          <a target={"_blank"}>
+            <Image src="/footer/twitter.svg" alt="twitter" height={40} width={40} />
+          </a>         
+        </Link>
+      </div>
+
+    </footer>
+  );
+}
+
+export default Footer;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Layout from '../components/Layout'
+import Footer from '../components/Footer'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-    
+    <>
+      <Component {...pageProps} /> 
+      <Footer />
+    </>
   )
 }
 
 export default MyApp
+ 
