e1f97c4cb888537cbd6b0d24778f6f4166d773fd
thestraightfrenchguy <edouard.finet@hotmail.co.uk>
2022-06-03 16:39:18 +0100

    slightly fix the background image (still not rotated)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,11 +8,11 @@ import Image from 'next/image'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <HeaderSmall />
+      <HeaderBig />
       <Component {...pageProps} /> 
       
       {/* I cant figure out how to rotate this image like in the design */}
-      <Image src={"/background-squiggle.png"} alt="background squigle" layout='fill' className="background-squiggle" ></Image>
+      <Image src={"/background-squiggle.png"} alt="background squigle" height={1752} width={405} className="background-squiggle" ></Image>
       
       <Footer />
     </>
