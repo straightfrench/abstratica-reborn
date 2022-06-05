5a792993e65537377adc5b40e210a7efedf3f04b
thestraightfrenchguy <edouard.finet@hotmail.co.uk>
2022-06-05 21:06:45 +0100

    dont put header in app level render as it changes beased on page.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Footer from '../components/Footer'
-import HeaderBig from '../components/HeaderBig'
-import HeaderSmall from '../components/HeaderSmall'
 import Image from 'next/image'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <HeaderBig />
       <Component {...pageProps} /> 
       
       {/* I cant figure out how to rotate this image like in the design */}
