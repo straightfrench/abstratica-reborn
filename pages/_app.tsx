f00adb442e5a735e62c26319a0df4d790f808c0b
thestraightfrenchguy <edouard.finet@hotmail.co.uk>
2022-06-09 18:52:25 +0100

    improve the background image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,11 +6,8 @@ import Image from 'next/image'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} /> 
-      
-      {/* I cant figure out how to rotate this image like in the design */}
-      <Image src={"/background-squiggle.png"} alt="background squigle" height={1752} width={405} className="background-squiggle" ></Image>
-      
+      <section className='background-image' />
+      <Component {...pageProps} />
       <Footer />
     </>
   )
