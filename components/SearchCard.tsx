f6348c0c5470278d883a38191d17658dc26304f4
thestraightfrenchguy <edouard.finet@hotmail.co.uk>
2022-06-15 17:46:03 +0100

    Add ability to swap between full collection and my collection.

--- components/SearchCard.tsx
+++ components/SearchCard.tsx
@@ -1,35 +1,36 @@
 import styles from "../styles/SearchCard.module.css"
 
-const SearchCard = () => {
+const FULL_COLLECTION = false;
+const OWNED_COLLECTION = true;
+
+const SearchCard = (props: { setCollection: (arg0: any) => void; }) => {
   return (
     <div className={styles["search-card"]}>
-      <form>
-        <button className={styles["collection-button"]} >Full collection</button>
-        <button className={styles["collection-button"]} >My collection</button>
-        <div>
-          <label>Frame</label>
-          <select name="frames" id="frame">
-            <option value={"none"}>Plain</option>
-            <option value={"bronze"}>Bronze</option>
-            <option value={"silver"}>Silver</option>
-            <option value={"gold"}>Gold</option>
-            <option value={"solana"}>Solana</option>
-          </select>
-        </div>
-        <div>
-          <label>Reflection</label>
-          <select name="reflections" id="reflection">
-            <option value={"none"}>Plain</option>
-            <option value={"silver"}>Silver</option>
-            <option value={"gold"}>Gold</option>
-            <option value={"solana"}>Solana</option>
-          </select>
-        </div>
-        <div>
-          <label>Abstraction</label>
-          <input placeholder="Number (syntax: 1,2,7-13)"></input>
-        </div>
-      </form>
+      <button onClick={ () => { props.setCollection(FULL_COLLECTION) } }>Full collection</button>
+      <button onClick={ () => { props.setCollection(OWNED_COLLECTION) } }>My collection</button>
+      <div>
+        <label>Frame</label>
+        <select name="frames" id="frame">
+          <option value={"none"}>Plain</option>
+          <option value={"bronze"}>Bronze</option>
+          <option value={"silver"}>Silver</option>
+          <option value={"gold"}>Gold</option>
+          <option value={"solana"}>Solana</option>
+        </select>
+      </div>
+      <div>
+        <label>Reflection</label>
+        <select name="reflections" id="reflection">
+          <option value={"none"}>Plain</option>
+          <option value={"silver"}>Silver</option>
+          <option value={"gold"}>Gold</option>
+          <option value={"solana"}>Solana</option>
+        </select>
+      </div>
+      <div>
+        <label>Abstraction</label>
+        <input placeholder="Number (syntax: 1,2,7-13)"></input>
+      </div>
     </div>
   );
 }
--- pages/collection.tsx
+++ pages/collection.tsx
@@ -1,16 +1,66 @@
 import HeaderSmall from "../components/HeaderSmall";
 import AbstraticaCard from "../components/AbstraticaCard";
-import SearchCard from "../components/SearchCard";
 import styles from "../styles/Collection.module.css"
+import { FC, useState, useEffect } from "react";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import SearchCard from "../components/SearchCard";
+import { WalletConnectButton, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
+const FullCollection: FC = () => {
+    
+  const { connection } = useConnection();
+  const { publicKey, sendTransaction } = useWallet();
+  const [ collection , setCollection ] = useState(false);
+
+  const creator = "absF5t1MP7dXFhbgUgaTi7ffJM2WhBg1cmrY5s7h3By";
 
 
-const FullCollection = () => {
-  return (
+  useEffect( () => {
+    console.log(collection)
+  }, [collection]); // [collection] is a dependancy array, useEffect will only run when collection changes.
+
+  
+  if (collection) {
+    if( !connection || !publicKey) {
+      return(
+        <div>
+          <HeaderSmall />
+          <div className={styles.container}>
+            <h1 className="section-title text-shadow" id={styles["collection-title"]}>My Abstractions</h1>
+            <div className={styles["search-container"]}>
+              <SearchCard setCollection={setCollection}/>
+            </div>
+            <div className="wallet-alert">
+              <WalletMultiButton />
+            </div>
+          </div>
+        </div>
+      )
+    }
+    return (
+      <div>
+        <HeaderSmall />
+        <div className={styles.container}>
+          <h1 className="section-title text-shadow" id={styles["collection-title"]}>My Abstractions</h1>
+          <div className={styles["search-container"]}>
+          <SearchCard setCollection={setCollection}/>
+          </div>
+          
+          <div className={styles["art-container"]}>
+            <AbstraticaCard />
+          </div>
+        </div>
+      </div>
+    )
+  }
+  else return (
     <div>
       <HeaderSmall />
       <div className={styles.container}>
-        <h1 className="section-title text-shadow" id={styles["full-collection-title"]}>The Full Collection</h1>
-        <div className={styles["search-container"]}><SearchCard /></div>
+        <h1 className="section-title text-shadow" id={styles["collection-title"]}>The Full Collection</h1>
+        <div className={styles["search-container"]}>
+        <SearchCard setCollection={setCollection}/>
+        </div>
         
         <div className={styles["art-container"]}>
           <AbstraticaCard />
@@ -54,5 +104,6 @@ const FullCollection = () => {
     </div>
   );
 }
+
 
 export default FullCollection;
